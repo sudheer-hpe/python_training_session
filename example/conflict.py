d28fb5444cec18cc934f84107fa3f70bc70deb69
aishwarya <aishwaryar@redcloudcomputing.com>
2024-10-01 17:05:26 +0530

    First commit after cloning

--- example/conflict.py
+++ example/conflict.py
@@ -4,7 +4,7 @@ This script demonstrates functionality in file1.
 def feature_file1():
     print("Feature in file - main branch")
     print("hello")
-    
+    print("I made some changes in this file after cloning it to my local machine")
     
 def test_python():
     print("Feature in file - main branch")
