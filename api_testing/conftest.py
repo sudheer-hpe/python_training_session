c98db18a9e65804b332c40251fd6284d5c34dc54
Sudheer Kumar P <sudheerkumar.peddathimmareddy-ext@hpe.com>
2024-10-24 12:37:18 +0530

    Latest changes.

--- api_testing/conftest.py
+++ api_testing/conftest.py
@@ -1,8 +1,19 @@
+import pytest
 import requests
+
+BASE_URL = "http://localhost:8080"
+
+@pytest.fixture
+def create_database():
+    return requests.get(f"{BASE_URL}/create-database")
+    # OR
+    # resp = requests.get(f"{BASE_URL}/create-database")
+    # return resp
+
 
 class APISession:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def send_request(self, request_type="GET", endpoint=""):
-        return requests.request(request_type)
+    def send_request(self, request_type="GET", endpoint="", data=None, params=None):
+        return requests.request(request_type, f"{self.base_url}/{endpoint}", data=data, params=params)
--- api_testing/test_api.py
+++ api_testing/test_api.py
@@ -1,1 +1,14 @@
-def test_create_database():
+from api_testing.conftest import APISession, BASE_URL
+
+
+def test_create_database(create_database):
+    response = create_database
+    assert response.status_code == 200, "Database Creation failed!!"
+    assert "Database created successfully" in response.text, "Response text mismatch!!"
+
+
+def test_add_entry():
+    api_session = APISession(BASE_URL)
+    endpoint = "entry"
+    response = api_session.send_request("POST", endpoint)
+    assert response.status_code == 200, ""
